bc71215e77f0d63b4fd35666113758854d11170d
Alexander Schwartzberg <aeksco@gmail.com>
2019-02-06 19:47:03 -0500

    Removed attribute from @codotype/types DEFAULT_SCHEMA

--- packages/datatypes/lib/default_schema.js
+++ packages/datatypes/lib/default_schema.js
@@ -8,18 +8,7 @@ const NEW_SCHEMA_MODEL = {
   class_name: '',
   class_name_plural: '',
   relations: [],
-  attributes: [
-    { // TODO - ditch default attribute?
-      order: 0,
-      label: 'Label',
-      identifier: 'label',
-      help: '',
-      required: true,
-      unique: true,
-      datatype: 'TEXT', // TODO - reference
-      datatypeOptions: {}
-    }
-  ]
+  attributes: []
 }
 
 module.exports = {
