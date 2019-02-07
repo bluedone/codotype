d92437256c13eeaeda643e4ce0062287d88fcfee
Alexander Schwartzberg <aeksco@gmail.com>
2019-02-06 19:44:13 -0500

    Relation module implements newRelation action

--- packages/ui/src/components/BlueprintEditor/store/relationModule.js
+++ packages/ui/src/components/BlueprintEditor/store/relationModule.js
@@ -4,6 +4,12 @@ import { DEFAULT_RELATION } from '@codotype/types/lib/default_relation'
 
 export default {
   namespaced: true,
+  actions: {
+    newRelation ({ commit }) {
+      commit('collection/resetNewModel')
+      commit('modals/form/showing', true)
+    }
+  },
   modules: {
     collection: Object.assign({}, collectionModule({ NEW_MODEL: DEFAULT_RELATION })),
     modals: {
