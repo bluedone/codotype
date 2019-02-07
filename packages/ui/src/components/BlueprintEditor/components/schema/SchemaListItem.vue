3d555618c318f99643430459095b0b3d24ad4354
Alexander Schwartzberg <aeksco@gmail.com>
2019-02-06 22:58:58 -0500

    SchemaListItem UI tweaks

--- packages/ui/src/components/BlueprintEditor/components/schema/SchemaListItem.vue
+++ packages/ui/src/components/BlueprintEditor/components/schema/SchemaListItem.vue
@@ -1,10 +1,34 @@
 <template>
-  <b-list-group-item
+  <li
     @click="selectModel(model)"
-    :active="isSelected"
+    :class="className"
   >
-    {{ model.label }}
-  </b-list-group-item>
+
+    <div class="row align-items-center">
+      <div class="col-md-8">
+        {{ model.label }}
+      </div>
+
+      <div class="col-md-4 text-right">
+        <span class="badge badge-dark" v-if="!model.attributes.length" v-b-tooltip.hover.left :title="model.attributes.length + (model.attributes.length === 1 ? ' Attribute' : ' Attributes')">
+          <i class="fa fa-times mr-1"></i>
+          0
+        </span>
+
+        <span class="badge badge-dark" v-else v-b-tooltip.hover.left :title="model.attributes.length + (model.attributes.length === 1 ? ' Attribute' : ' Attributes')">
+          <i class="fa fa-tags mr-1"></i>
+          {{ model.attributes.length }}
+        </span>
+
+        <!-- <span class="badge badge-dark" v-b-tooltip.hover.right :title="model.relations.length + (model.relations.length === 1 ? ' Relation' : ' Relations')"> -->
+          <!-- <i class="fa fa-link mr-1"></i> -->
+          <!-- {{ model.relations.length }} -->
+        <!-- </span> -->
+      </div>
+
+    </div>
+
+  </li>
 </template>
 
 <script>
@@ -20,10 +44,28 @@ export default {
   computed: {
     isSelected () {
       return this.model.id === this.$store.getters['editor/schema/selectedModel/id']
-    }
+    },
+    className () {
+      let css = ['list-group-item', 'list-group-item-action']
+
+      if (this.isSelected) {
+        // css.push('active')
+        css.push('list-group-item-primary')
+      }
+
+      return css.join(' ')
+    },
   },
   methods: mapActions({
     selectModel: 'editor/schema/selectModel'
   })
 }
 </script>
+
+<style lang="sass" scoped>
+  li.list-group-item
+    cursor: pointer
+
+  a.row.align-items-center
+    text-decoration: none
+</style>
