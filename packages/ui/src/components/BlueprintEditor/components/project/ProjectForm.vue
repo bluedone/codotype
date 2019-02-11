6ef94ba827ebeb4c51fa80c375ff8a82d7389545
Alexander Schwartzberg <aeksco@gmail.com>
2019-02-10 23:59:54 -0500

    ProjectForm spacing fixes in @codotype/ui

--- packages/ui/src/components/BlueprintEditor/components/project/ProjectForm.vue
+++ packages/ui/src/components/BlueprintEditor/components/project/ProjectForm.vue
@@ -1,8 +1,13 @@
 <template>
-  <b-row class='align-items-center'>
+  <b-row class='align-items-center w-100 justify-content-center'>
     <b-col lg=6>
-      <h4 class="text-muted">Getting Started</h4>
-      <p class="lead form-text text-muted">Give a name to your project - try something simple, like "ToDo List", or "Inventory Manager". No need to be picky, you can change it later.</p>
+      <h4>Let's give your project a name</h4>
+      <p class="lead form-text text-muted">Try something simple, like <code>ToDo List</code>, or <code>Inventory Manager</code>
+
+      </p>
+
+      <p>No need to be picky, you can change it later.</p>
+
       <small class="text-muted">The Project Name will appear in the header of the app we're building - you can change it later.</small>
     </b-col>
     <b-col lg=6>
