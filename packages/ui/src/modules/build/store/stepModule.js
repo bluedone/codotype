90fd6cef4160dfe46833bb66f73cca4e18f1900b
Alexander Schwartzberg <aeksco@gmail.com>
2019-02-12 17:50:25 -0500

    StepModule - added annotations for incoming changes

--- packages/ui/src/modules/build/store/stepModule.js
+++ packages/ui/src/modules/build/store/stepModule.js
@@ -1,8 +1,19 @@
-// Vuex module to manage build steps
+import collectionModule from '../../../store/lib/collectionModule'
+
+// TODO - clean up the rest of this vuex module
 export default {
   namespaced: true,
   state: {
     current: 0
+  },
+  actions: {
+    selectGenerator ({}, generator) {
+      // TODO - implement BuildStep factory logic
+      // PROJECT STEP ALWAYS PRESENT
+      // if generator.self_configuring ----> NO SCHEMA EDITOR STEP
+      // if NOT generator.option_groups[0] ----> NO GENERATOR CONFIGURATION STEP
+      // QUESTION - what happens if there's a generator that's self-configuring, with no option_groups?
+    }
   },
   mutations: {
     current (state, current) {
@@ -27,6 +38,9 @@ export default {
     jumpTo ({ state, commit }, step) {
       commit('current', step)
     }
+  },
+  modules: {
+    // TODO - complete implementation of step/collection module
+    collection: Object.assign({}, collectionModule({ NEW_MODEL: {} }))
   }
-
 }
