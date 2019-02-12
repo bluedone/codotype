098d876b6e1123357006b3e9cf5ac99fb32af89a
Alexander Schwartzberg <aeksco@gmail.com>
2019-02-12 18:05:29 -0500

    BuildModule - added annotations for incoming changes

--- packages/ui/src/modules/build/store/index.js
+++ packages/ui/src/modules/build/store/index.js
@@ -1,13 +1,8 @@
-// import actions from './actions'
-// import state from './state'
 import stepModule from './stepModule'
 import runtimeModule from './runtimeModule'
-const namespaced = true
 
 export default {
-  namespaced,
-  // state,
-  // actions,
+  namespaced: true,
   modules: {
     steps: stepModule,
     runtime: runtimeModule
--- packages/ui/src/modules/generator/router.js
+++ packages/ui/src/modules/generator/router.js
@@ -6,6 +6,9 @@ export default [
     path: '/generators',
     component: GeneratorList
   },
+  // TODO - add an additional page - generators/:id/build
+  // This specific route should encapsulate a series of sub-routes
+  // for the GeneratorShow and GeneratorBuild pages
   {
     path: '/generators/:id',
     component: GeneratorShow,
