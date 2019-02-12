6ebc8f7ed8e7bf08534113f53a95ff96eeaabb51
Alexander Schwartzberg <aeksco@gmail.com>
2019-02-12 17:55:47 -0500

    Added build/runtime module - includes annotations for incoming changes

--- packages/ui/src/modules/build/store/index.js
+++ packages/ui/src/modules/build/store/index.js
@@ -1,17 +1,15 @@
 // import actions from './actions'
 // import state from './state'
-// import getters from './getters'
-// import mutations from './mutations'
 import stepModule from './stepModule'
+import runtimeModule from './runtimeModule'
 const namespaced = true
 
 export default {
   namespaced,
   // state,
-  // mutations,
   // actions,
-  // getters,
   modules: {
-    steps: stepModule
+    steps: stepModule,
+    runtime: runtimeModule
   }
 }
