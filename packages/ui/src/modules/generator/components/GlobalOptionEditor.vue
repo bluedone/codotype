3e5639ef8436ef7952093456befaf35926948c4c
Alexander Schwartzberg <aeksco@gmail.com>
2019-02-13 17:09:05 -0500

    Split up ConfigureGenerator into a series of smaller components

---
+++ packages/ui/src/modules/generator/components/GlobalOptionEditor.vue
@@ -0,0 +1,41 @@
+<!-- TODO - this should really be an entire separate Editor component -->
+<!-- Should not even be SCOPED to the generator module -->
+<template>
+  <b-row class='justify-content-center'>
+    <b-col lg=9>
+
+      <!-- Header - Description -->
+      <!-- <small class="text-muted">{{group.description}}</small> -->
+
+      <!-- Define new instance -->
+      <div
+        class="card card-body mt-2"
+        v-for="attr in group.attributes"
+        :key="attr.identifier"
+      >
+        <OptionFormItem
+          :group="group"
+          :model="attr"
+        />
+      </div>
+
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+import OptionFormItem from '../../option/components/OptionFormItem'
+import OptionTemplateRenderer from '../../option/components/OptionTemplateRenderer'
+
+export default {
+  name: 'GlobalOptionEditor',
+  props: {
+    group: { required: true }
+  },
+  components: {
+    OptionTemplateRenderer,
+    OptionFormItem
+  }
+}
+</script>
