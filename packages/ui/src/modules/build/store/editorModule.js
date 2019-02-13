a77851631c810c7580e2d312c63fa16391a153bf
Alexander Schwartzberg <aeksco@gmail.com>
2019-02-12 23:28:03 -0500

    Build Vuex module taking shape with dynamic Vuex modules

--- packages/ui/src/modules/build/store/editorModule.js
+++ packages/ui/src/modules/build/store/editorModule.js
@@ -1,41 +1,112 @@
 // import cloneDeep from 'lodash/cloneDeep'
 // import buildConfiguration from '@codotype/util/lib/buildConfiguration'
 import collectionModule from '../../../store/lib/collectionModule'
+import objectModule from './objectModule'
+import addonModule from './addonModule'
+
+// const setValue = (state, { group, attr, val }) => { state.config[group][attr] = val }
 
 // NOTE - the editor module is ONLY responsible for editing the build.configuration property
 export default {
   namespaced: true,
-  state: {
-    exists: true
-  },
   actions: {
-    selectBuild ({ state, rootGetters, commit, dispatch }, generator_id) {
-      // Checks to ensure this generator isn't already in the build
-      // if (state.newModel.stages.map(stage => stage.generator_id).includes(generator_id)) return
+    // LOAD a build configuration into the editor
+    // The editor is NOT responsible for creating the configuration object...?
+    load ({ commit, dispatch }, { generator_option_groups, schemas }) {
+      generator_option_groups.forEach((og) => dispatch('registerDynamicModule', { schemas, option_group: og }))
+    },
+    // CLEARS the editor module state
+    // De-registers all dynamic modules
+    clear ({}) {
+      console.log('CLEAR EDITOR MODULE HERE')
+    },
+    registerDynamicModule ({ state }, { option_group, schemas }) {
 
-      // Creates newStage, assigns generator_id
-      // const newStage = cloneDeep(DEFAULT_BUILD_STAGE)
-      // newStage.generator_id = generator_id
+      // TODO - constantize GLOBAL_OPTION type
+      if (option_group.type === 'OPTION_GROUP_TYPE_GLOBAL_OPTION') {
 
-      // // // //
-      // Generates stage configuration object
-      // TODO - abstract into @codotype/util??
-      // const generatorMeta = rootGetters['generator/collection'].find(g => g.id === generator_id)
+        // Defines the default state
+        // TODO - this must be moved into @codotype/util
+        const defaultState = {}
+        option_group.attributes.forEach((attr) => {
+          defaultState[attr.identifier] = attr.default_value
+        })
 
-      // Pulls the blueprint to define the build configuration
-      // const blueprint = rootGetters['blueprint/selectedModel']
+        // TODO - this should accept the attributes of the option group, yeah?
+        // All validations can be managed in there?
+        const newModule = objectModule({ defaultState: defaultState })
 
-      // Generates the stage's configuration from the selected generator
-      // newStage.configuration = buildConfiguration({ schemas: blueprint.schemas, generator: generatorMeta })
+        // Defines the scope of the module
+        const scope = option_group.identifier
+        this.registerModule(['build', 'editor', scope], newModule)
+      // TODO - constantize global option here
+      } else if (option_group.type === 'OPTION_GROUP_TYPE_MODEL_OPTION') {
+        // New empty module to wrap an ObjectModule for each schema
+        const newModule = { modules: {} }
 
-      // once it's been created in the `stage` module
-      // newModel.stages.push(newStage)
-      // commit('newModel', newModel)
+        // Defines the default state
+        // TODO - this must be moved into @codotype/util
+        const defaultState = {}
+        option_group.attributes.forEach((attr) => {
+          defaultState[attr.identifier] = attr.default_value
+        })
+
+        // Defines schema-level modules
+        schemas.forEach((s) => {
+          newModule.modules[s.identifier] = objectModule({ defaultState: defaultState })
+          console.log('Added object module for ', s.identifier)
+        })
+
+        // Defines the scope of the module
+        console.log(option_group)
+        const scope = option_group.identifier
+        this.registerModule(['build', 'editor', scope], newModule)
+      // HANDLES OPTION_GROUP_TYPE_ADDON
+      } else if (option_group.type === 'OPTION_GROUP_TYPE_GLOBAL_ADDON') {
+        console.log('HANDLE ADDON HERE')
+        const scope = option_group.identifier_plural
+        this.registerModule(['build', 'editor', scope], collectionModule({ NEW_MODEL: {} })) // TODO - pass in newModel from @codotype/ui
+      } else if (option_group.type === 'OPTION_GROUP_TYPE_MODEL_ADDON') {
+        // New empty module to wrap an ObjectModule for each schema
+        const newModule = { modules: {} }
+
+        // Defines the default state
+        // TODO - this must be moved into @codotype/util
+        const defaultState = {}
+        option_group.attributes.forEach((attr) => {
+          defaultState[attr.identifier] = attr.default_value
+        })
+
+        // Defines schema-level modules
+        // NOTE - a full-blown collectionModule might not be ideal here
+        // Would be best to issue a stripped-down module, and be able to select
+        schemas.forEach((s) => {
+          newModule.modules[s.identifier] = addonModule()
+          console.log('Added object module for ', s.identifier)
+        })
+
+        // Defines the scope of the module
+        console.log(option_group)
+        const scope = option_group.identifier
+        this.registerModule(['build', 'editor', scope], newModule)
+      }
+
+      // QUESTION - do we want { preserveState: true } ?
+      // const newModule = Object.assign({}, collectionModule({ NEW_MODEL: {} }))
+
+      // Registers the new Vuex module
+      // QUESTION - do we need to de-register it as well?
+      // this.registerModule(['build', 'editor', scope], newModule)
     }
   },
-  mutations: {},
-  getters: {},
+  getters: {
+    // TODO - this will pull out ONLY what's needed from the modules
+    // NOTE - this will NEED to reference option_groups and schemas
+    toObj: state => {
+      return state
+    }
+  },
   modules: {
-    addon: Object.assign({}, collectionModule({ NEW_MODEL: {} }))
+    // addon: Object.assign({}, collectionModule({ NEW_MODEL: {} })) // This is used to edit the CURRENT addon, is it needed?
   }
 }
--- packages/ui/src/modules/build/store/index.js
+++ packages/ui/src/modules/build/store/index.js
@@ -3,9 +3,56 @@ import stepModule from './stepModule'
 import runtimeModule from './runtimeModule'
 import editorModule from './editorModule'
 import collectionModule from '../../../store/lib/collectionModule'
+import buildConfiguration from '@codotype/util/lib/buildConfiguration'
 
 export default {
   namespaced: true,
+  actions: {
+    // NOTE - a lot of the process here may need to be replicated elsewhere - something to think about moving forward...
+    selectBuild ({ state, getters, rootGetters, commit, dispatch }, generator_id) {
+      // FIND OR CREATES BUILD MODEL
+      let selectedBuild = getters['collection/items'].find(s => s.generator_id === generator_id)
+
+      // Defines a new build if one does not exist for the selected generator (generator_id)
+      if (!selectedBuild) {
+        console.log('MISSING BUILD - CREATE AND RE-RUN')
+        // Pulls in generatorMeta
+        // const generatorMeta = rootGetters['generator/collection'].find(g => g.id === generator_id)
+        // TODO - throw an error here if no generator has been found
+
+        // Pulls in schemas from editor/schema
+        // const schemas = rootGetters['editor/schema/collection/items']
+        // console.log(schemas)
+
+        // Defines a new Build model
+        const newModel = Object.assign({}, DEFAULT_BUILD)
+        newModel.generator_id = generator_id
+        newModel.configuration = {}
+        // TODO - the new implementation doesn't rely on buildConfiguration quite as directly
+        // newModel.configuration = buildConfiguration({ schemas: schemas, generator: generatorMeta })
+
+        // Saves the new model and re-dispatches this action
+        commit('collection/newModel', newModel)
+        dispatch('collection/create')
+        return dispatch('selectBuild', generator_id)
+      }
+
+      // QUESTION - how to handle the case where a build is being reloaded, but the schemas have changed?
+      // If selectedBuild is found, there should be a merge function that can runs buildConfiguration for a new
+      // configuration for the editor, and merges that with the previous one.
+      // *Should exist in @codotype/util
+
+      // Clears the current editor (TODO - it might be )
+      dispatch('editor/clear')
+
+      // Loads the selectedBuild into the editor
+      const generatorMeta = rootGetters['generator/collection'].find(g => g.id === generator_id)
+      return dispatch('editor/load', {
+        schemas: rootGetters['editor/schema/collection/items'],
+        generator_option_groups: generatorMeta.option_groups
+      })
+    }
+  },
   modules: {
     collection: Object.assign({}, collectionModule({ NEW_MODEL: DEFAULT_BUILD })),
     steps: stepModule,
