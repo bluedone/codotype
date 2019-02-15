ac1e80758a5366b1ec1b9a33d61a30d0b099d870
Alexander Schwartzberg <aeksco@gmail.com>
2019-02-15 15:51:07 -0500

    Removed deprecated & unused Option Vuex store

--- packages/ui/src/modules/option/store/constants.js
+++ packages/ui/src/modules/option/store/constants.js
@@ -13,30 +13,3 @@ export const DEFAULT_OPTION = {
   default_value: true,
   datatypeOptions: {}
 }
-
-// Datattype IDs
-// TODO - these are duplicated in the Option store
-export const TEXT = 'TEXT'
-export const TEXT_SELECT = 'TEXT_SELECT'
-export const NUMBER = 'NUMBER'
-export const NUMBER_SELECT = 'NUMBER_SELECT'
-export const BOOL = 'BOOL'
-export const DATE = 'DATE'
-export const TIME = 'TIME'
-export const DATETIME = 'DATETIME'
-export const COLOR = 'COLOR'
-
-// TODO - move into @codotype/types
-export const OPTION_TYPES = [
-  { value: TEXT, text: 'Text', icon: 'fa-quote-left' },
-  { value: NUMBER, text: 'Number', icon: 'fa-hashtag' },
-  { value: BOOL, text: 'Boolean', icon: 'fa-check-square-o' },
-  // { value: DATE, text: 'Date', icon: 'fa-calendar-o' },
-  // { value: TIME, text: 'Time', icon: 'fa-clock-o' },
-  // { value: DATETIME, text: 'DateTime', icon: 'fa-calendar-plus-o' },
-  { value: COLOR, text: 'Color', icon: 'fa-eyedropper' },
-  { value: NUMBER_SELECT, text: 'Number Select', icon: 'fa-list' },
-  { value: TEXT_SELECT, text: 'Text Select', icon: 'fa-list' }
-  // { value: 'JSON', text: 'JSON', icon: 'fa-code' }
-  // { value: 'GEO', text: 'Geolocation', icon: 'fa-map' },
-]
