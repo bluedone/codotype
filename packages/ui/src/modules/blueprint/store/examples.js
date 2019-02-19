04389483e34fedad2a84ca643bbf9adecc4de7a5
Alexander Schwartzberg <aeksco@gmail.com>
2019-02-19 13:47:50 -0500

    Rescoped @codotype/types package

--- packages/ui/src/modules/blueprint/store/examples.js
+++
@@ -1,9 +0,0 @@
-// import TODO_EXAMPLE from '@codotype/blueprints/lib/todo-list'
-// import LIBRARY_EXAMPLE from '@codotype/blueprints/lib/textbook-library'
-// import TWEETER_EXAMPLE from '@codotype/blueprints/lib/twitter'
-
-export const EXAMPLE_COLLECTION = [
-  // TODO_EXAMPLE,
-  // LIBRARY_EXAMPLE,
-  // TWEETER_EXAMPLE
-]
