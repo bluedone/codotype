5515790f45ded93b3b91f3b32035be4b28ba837c
Alexander Schwartzberg <aeksco@gmail.com>
2019-03-09 13:07:03 -0500

    UI package config cleanup

--- packages/@codotype/ui/src/config/index.js
+++ packages/@codotype/ui/src/config/index.js
@@ -1,35 +1,16 @@
-// import Vue from 'vue'
-// import Vuex from 'vuex'
+// TODO - rename this file to plugins
 import Meta from 'vue-meta'
 import BootstrapVue from 'bootstrap-vue'
 import ToggleButton from 'vue-js-toggle-button'
 import VueGitHubButtons from 'vue-github-buttons'
+import VRuntimeTemplate from 'v-runtime-template'
 import 'vue-github-buttons/dist/vue-github-buttons.css'
 import 'driver.js/dist/driver.min.css'
-// import 'vue-step-indicator/dist/vue-step-indicator.css'
-
-// bootstrap-vue
-// Bootstrap components and directives
-// Vue.use(BootstrapVue)
-
-// vue-meta
-// supports `meta` object returned with `module.defaults`
-// Vue.use(Meta)
-
-// vue-toggle-button
-// http://vue-js-toggle-button.yev.io/
-// Vue.use(ToggleButton)
-
-// vuex
-// State management library
-// Vue.use(Vuex)
-
-// vue-github-buttons
-// Vue.use(VueGitHubButtons)
 
 export default {
   VueGitHubButtons,
   BootstrapVue,
   ToggleButton,
-  Meta
+  Meta,
+  VRuntimeTemplate
 }
