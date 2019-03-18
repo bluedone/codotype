53d8fee74c304cb36d513816cf694debc4a556a7
Alexander Schwartzberg <aeksco@gmail.com>
2019-03-18 17:50:59 -0400

    Datatypes package - renamed text attribute to label

--- packages/@codotype/datatypes/lib/meta.js
+++ packages/@codotype/datatypes/lib/meta.js
@@ -15,20 +15,20 @@ const {
 
 // Defines datatype metadata
 const DATATYPE_META = {}
-DATATYPE_META[DATATYPE_STRING] = { value: DATATYPE_STRING, text: 'String', icon: 'fa fa-quote-left' },
-DATATYPE_META[DATATYPE_TEXT] = { value: DATATYPE_TEXT, text: 'Text', icon: 'fa fa-quote-left' },
-DATATYPE_META[DATATYPE_STRING_ARRAY] = { value: DATATYPE_STRING_ARRAY, text: 'String Array', icon: 'fa fa-quote-left' },
+DATATYPE_META[DATATYPE_STRING] = { value: DATATYPE_STRING, label: 'String', icon: 'fa fa-quote-left' },
+DATATYPE_META[DATATYPE_TEXT] = { value: DATATYPE_TEXT, label: 'Text', icon: 'fa fa-quote-left' },
+DATATYPE_META[DATATYPE_STRING_ARRAY] = { value: DATATYPE_STRING_ARRAY, label: 'String Array', icon: 'fa fa-quote-left' },
 
-DATATYPE_META[DATATYPE_BOOLEAN] = { value: DATATYPE_BOOLEAN, text: 'Boolean', icon: 'far fa-check-square' },
+DATATYPE_META[DATATYPE_BOOLEAN] = { value: DATATYPE_BOOLEAN, label: 'Boolean', icon: 'far fa-check-square' },
 
-DATATYPE_META[DATATYPE_INTEGER] = { value: DATATYPE_INTEGER, text: 'Integer', icon: 'fa fa-hashtag' },
-DATATYPE_META[DATATYPE_FLOAT] = { value: DATATYPE_FLOAT, text: 'Float', icon: 'fa fa-hashtag' },
-DATATYPE_META[DATATYPE_DOUBLE] = { value: DATATYPE_DOUBLE, text: 'Double', icon: 'fa fa-hashtag' },
+DATATYPE_META[DATATYPE_INTEGER] = { value: DATATYPE_INTEGER, label: 'Integer', icon: 'fa fa-hashtag' },
+DATATYPE_META[DATATYPE_FLOAT] = { value: DATATYPE_FLOAT, label: 'Float', icon: 'fa fa-hashtag' },
+DATATYPE_META[DATATYPE_DOUBLE] = { value: DATATYPE_DOUBLE, label: 'Double', icon: 'fa fa-hashtag' },
 
-DATATYPE_META[DATATYPE_DATE] = { value: DATATYPE_DATE, text: 'Date', icon: 'far fa-calendar' },
-DATATYPE_META[DATATYPE_TIME] = { value: DATATYPE_TIME, text: 'Time', icon: 'far fa-clock' },
-DATATYPE_META[DATATYPE_DATETIME] = { value: DATATYPE_DATETIME, text: 'DateTime', icon: 'far fa-calendar-alt' },
+DATATYPE_META[DATATYPE_DATE] = { value: DATATYPE_DATE, label: 'Date', icon: 'far fa-calendar' },
+DATATYPE_META[DATATYPE_TIME] = { value: DATATYPE_TIME, label: 'Time', icon: 'far fa-clock' },
+DATATYPE_META[DATATYPE_DATETIME] = { value: DATATYPE_DATETIME, label: 'DateTime', icon: 'far fa-calendar-alt' },
 
-DATATYPE_META[DATATYPE_JSON] = { value: DATATYPE_JSON, text: 'JSON', icon: 'fa fa-code' }
+DATATYPE_META[DATATYPE_JSON] = { value: DATATYPE_JSON, label: 'JSON', icon: 'fa fa-code' }
 
 module.exports = DATATYPE_META
