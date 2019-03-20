4667c771ee587556b952825833c782cf106d2d88
Alexander Schwartzberg <aeksco@gmail.com>
2019-03-19 20:50:46 -0400

    @codotype/util - added buildDefault spec

--- packages/@codotype/util/test/index.js
+++ packages/@codotype/util/test/index.js
@@ -5,9 +5,6 @@ chai.should();
 // Import all library tests here
 require('../lib/buildDefault.spec');
 
-// Import all spec & integration tests here
-// require('../src/api/auth/auth.spec')
-
 // // // //
 
 // const fs = require('fs')
