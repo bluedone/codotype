ba73b0188e35a7533b283ca46167f553266cfc45
Alexander Schwartzberg <aeksco@gmail.com>
2019-03-19 21:26:13 -0400

    Added indent.spec to @codotype/util

--- packages/@codotype/util/test/index.js
+++ packages/@codotype/util/test/index.js
@@ -4,8 +4,7 @@ chai.should();
 
 // Import all library tests here
 require('../lib/buildDefault.spec');
+require('../lib/indent.spec');
 require('../lib/inflateMeta.spec');
 require('../lib/sanitizeLabel.spec');
 require('../lib/trailingComma.spec');
-
-// // // //
