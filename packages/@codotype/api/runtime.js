29d9f586afbed3ca7fcb96e480f3cd8650411c34
Alexander Schwartzberg <aeksco@gmail.com>
2019-03-21 03:32:50 -0400

    Updated API package runtime.js definition

--- packages/@codotype/api/runtime.js
+++ packages/@codotype/api/runtime.js
@@ -5,23 +5,16 @@ const CodotypeRuntime = require('@codotype/runtime');
 // Instantiates Codotype runtime
 const runtime = new CodotypeRuntime();
 
-// Registers generators
-// Ideally the runtime would be encapsulated in a docker container to separate things cleanly
-runtime.registerGenerator({ relative_path: './node_modules/codotype-mevn-generator' });
-// runtime.registerGenerator({ relative_path: './node_modules/codotype-vuejs-vuex-bootstrap-generator' });
-// runtime.registerGenerator({ relative_path: './node_modules/codotype-nodejs-express-mongodb-generator' });
-// runtime.registerGenerator({ relative_path: './node_modules/codotype-postman-collection-generator' });
-// runtime.registerGenerator({ relative_path: './node_modules/codotype-python-falcon-mongodb-generator' });
+// Registers local generators for development
+runtime.registerGenerator({ relative_path: '../codotype-hackathon-starter' });
+runtime.registerGenerator({ relative_path: '../codotype-postman-collection-generator' });
+runtime.registerGenerator({ relative_path: '../codotype-mevn-generator' });
+runtime.registerGenerator({ relative_path: '../codotype-python-falcon-mongodb-generator' });
+runtime.registerGenerator({ relative_path: '../codotype-mern-generator' });
+runtime.registerGenerator({ relative_path: '../codotype-vuejs-vuex-bootstrap-generator' });
+runtime.registerGenerator({ relative_path: '../codotype-nodejs-express-mongodb-generator' });
+runtime.registerGenerator({ relative_path: '../codotype-react-generator' });
 // runtime.registerGenerator({ relative_path: './node_modules/codotype-mongodb-scripts-generator' });
-
-// Local for developing...
-// runtime.registerGenerator({ relative_path: '../../../../codotype-mevn-generator' });
-// runtime.registerGenerator({ relative_path: '../../../codotype-vuejs-vuex-bootstrap-generator' });
-// runtime.registerGenerator({ relative_path: '../../../codotype-nodejs-express-mongodb-generator' });
-// runtime.registerGenerator({ relative_path: '../../../codotype-postman-collection-generator' });
-// runtime.registerGenerator({ relative_path: '../../../codotype-python-falcon-mongodb-generator' });
-// runtime.registerGenerator({ relative_path: '../../../codotype-hackathon-starter' });
-// runtime.registerGenerator({ relative_path: '../../../codotype-react-generator' });
 
 // // // //
 
