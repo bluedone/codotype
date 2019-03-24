9cdca9a50ac27b7e5161791eb7c941f9771bda0c
Alexander Schwartzberg <aeksco@gmail.com>
2019-03-23 20:32:31 -0400

    Updated datatype-meta, default_relation, and default_schema in @codotype/types

--- packages/@codotype/types/lib/default_relation.js
+++ packages/@codotype/types/lib/default_relation.js
@@ -1,12 +1,12 @@
 const { RELATION_TYPE_BELONGS_TO } = require('./relation-types')
 
 // Relation constants
-// QUESTION - add schema_id to this type?
 const DEFAULT_RELATION = {
   id: null,
   order: 0,
   type: RELATION_TYPE_BELONGS_TO,
   required: false,
+  schema_id: '',
   related_schema_id: '',
   reverse_relation_id: '',
   as: '', // TODO - rename 'as' to something else?
