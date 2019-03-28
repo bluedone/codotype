2e946f32cccecdd7f78fb44288cd1ac82b306f7a
Alexander Schwartzberg <aeksco@gmail.com>
2019-03-27 20:19:05 -0400

    Core generator features abstracted into runtime

--- packages/@codotype/generator/lib/generator.js
+++ packages/@codotype/generator/lib/generator.js
@@ -1,6 +1,5 @@
-const ejs = require('ejs')
-const path = require('path');
-const fsExtra = require('fs-extra')
+const ejs = require('ejs') // TODO - abstract into @codotype/runtime
+const path = require('path'); // TODO - abstract into @codotype/runtime
 const indent = require('@codotype/util/lib/indent')
 const trailingComma = require('@codotype/util/lib/trailingComma')
 const { buildDefault } = require('@codotype/util/lib/buildDefault')
@@ -14,7 +13,17 @@ module.exports = class CodotypeGenerator {
 
   // constructor
   // Handles build options
+  // TODO - pass in @codotype/runtime instance into this constructor (in options, maybe?)
   constructor (constructorOptions, options) {
+
+    // TODO - document this!
+    if (!options.runtime) {
+      throw Error('Generator must receive a runtime option')
+    }
+
+    // Assigns this.runtime
+    // this.runtime must be a compliant CodotypeRuntime class instance
+    this.runtime = options.runtime
 
     // Assigns constructorOptions
     this.write = constructorOptions.write || this.write
@@ -24,10 +33,8 @@ module.exports = class CodotypeGenerator {
     // Assigns this.options
     this.options = options;
 
-    // Assigns helper libraries to class variables
-    this.fs = fsExtra
-
     // Assigns buildDefault helpers from @codotype/util
+    // QUESTION - scope under helpers?
     this.buildDefault = buildDefault;
 
     // PASS this.options.resolved in from @codotype/runtime
@@ -53,36 +60,18 @@ module.exports = class CodotypeGenerator {
   // ensureDir
   // Ensures presence of directory for template compilation
   async ensureDir (dir) {
-    const destination_path = path.join(this.options.dest, dir)
-    return new Promise((resolve, reject) => {
-      return fsExtra.ensureDir(destination_path, (err) => {
-        if (err) return reject(err)
-        return resolve()
-      })
-    })
+    return this.runtime.ensureDir(this.destinationPath(dir))
   }
 
   // copyDir
-  // copy a directory from src to dest
+  // copy a directory from src to dest'
+  // TODO - abstract FS-level operations into @codotype/runtime
   async copyDir (src, dest) {
-    return new Promise((resolve, reject) => {
-      return this.fs.copy(src, dest, (err) => {
-        if (err) return reject(err)
-        return resolve()
-      })
-    })
+    return this.runtime.copyDir(src, dest)
   }
 
-  // compileInPlace
-  // Returns an array of filenames whos
-  // templatePath and destinationPath are identical
-  // Helpful for compiling lots of one-off templates without writing lots of machinery around them
-  // compileInPlace () {
-  //   return []
-  // }
-
   // compileTemplatesInPlace
-  // Compiles and writes each template defined in the `compileInPlace` method
+  // Compiles and writes each template defined in the `compileInPlace` property
   compileTemplatesInPlace () {
     // For each inPlaceTemplate, compile and write
     return Promise.all(this.compileInPlace.map((template) => {
@@ -94,7 +83,8 @@ module.exports = class CodotypeGenerator {
   }
 
   // renderComponent
-  // Compiles and writes each template defined in the `compileInPlace` method
+  // Compiles and writes each template defined in the `compileInPlace` property
+  // TODO - this should be updated to proxy `data` into the `copyTemplate` method
   renderComponent ({ src, dest, options = {} }) {
     // For each inPlaceTemplate, compile and write
     return this.copyTemplate(
@@ -104,66 +94,30 @@ module.exports = class CodotypeGenerator {
     )
   }
 
-  // renderTemplate
-  // Compiles an EJS template and returns the result
-  renderTemplate (src, options = {}) {
-    return new Promise((resolve, reject) => {
-
-      // default options padded into the renderFile
-      let renderOptions = {}
-
-      // // // //
-      // // // //
-      // TODO - document this structure
-      // The `data` object is passed into each file that gets rendered
-      // TODO - this should be abstracted into a separate function
-      const data = {
-        blueprint: this.options.blueprint,
-        meta: this.options.meta,
-        configuration: this.options.configuration,
-        helpers: {
-          indent,
-          trailingComma
-        },
-        ...datatypes,
-        ...relationTypes,
-        ...options
-      }
-      // // // //
-      // // // //
-
-      // Compiles EJS template
-      return ejs.renderFile(src, data, renderOptions, (err, str) => {
-
-        // Handles template compilation error
-        if (err) return reject(err)
-
-        // Resolves with compiled template
-        return resolve(str);
-
-      })
-
-    })
-  }
-
   // copyTemplate
   // Compiles a template and writes to the dest location
+  // TODO - split this up to rely on runtime methods instead of referencing FS directly
   copyTemplate (src, dest, options = {}) {
     // console.log('Copying:' + dest)
     return new Promise(async (resolve, reject) => {
 
-      // console.log('Rendering:' + dest)
-      const compiledTemplate = await this.renderTemplate(src, options)
-      // console.log('Rendered:' + dest)
+      // TODO - DOCUMENT!!!
+      // this.runtime.log('Rendering:' + dest)
 
+      // TODO - DOCUMENT!!!
+      const compiledTemplate = await this.runtime.renderTemplate(this, src, options)
+
+      // TODO - DOCUMENT!!!
+      // this.runtime.log('Rendered:' + dest)
+
+      // TODO - DOCUMENT!!!
       // Does the destination already exist?
-      const exists = fsExtra.existsSync(dest)
+      const exists = this.runtime.existsSync(dest)
 
+      // TODO - DOCUMENT!!!
       // If it doesn't exist, OKAY TO WRITE
       if (exists) {
-        const existing = fsExtra.readFileSync(dest, 'utf8')
-        // If exists, and it's the same, SKIP
-        if (compiledTemplate === existing) {
+        if (this.runtime.compareFile(dest, compiledTemplate)) {
           return resolve()
         } else {
           // TODO - this needs a GitHub issue
@@ -172,165 +126,30 @@ module.exports = class CodotypeGenerator {
       }
 
       // Writes the compiled template to the dest location
-      // console.log('Writing: ' + dest)
-      this.fs.writeFile(dest, compiledTemplate, (err) => {
-        if (err) return reject(err)
-        // console.log('Wrote: ' + dest)
-        // console.log('\n')
-        return resolve();
-      });
+      this.runtime.writeFile(dest, compiledTemplate).then(() => {
+        return resolve()
+      })
 
     })
   }
 
   // templatePath
-  // TODO - document
-  // TODO - document
+  // TODO - DOCUMENT!!
+  // TODO - DOCUMENT!!
   templatePath (template_path = './') {
-    return path.join(this.resolved, 'templates', template_path)
+    return this.runtime.templatePath(this.resolved, template_path)
   }
 
   // destinationPath
-  // TODO - document
-  // TODO - document
+  // TODO - DOCUMENT!!
+  // TODO - DOCUMENT!!
   destinationPath (destination_path = './') {
-    return path.join(this.options.dest, destination_path)
+    return this.runtime.destinationPath(this.options.dest, destination_path)
   }
 
   // composeWith
   // Enables one generator to fire off several child generators
-  // TODO - clean up + document this function
-  // TODO - clean up + document this function
-  // TODO - clean up + document this function
-  async composeWith (generatorModule, options={}) {
-
-    // Defines module path
-    let modulePath
-
-    // // // //
-    // // // //
-    // TODO - love into verbose logging function
-
-    // console.log('Compose with')
-    // console.log(generatorModule)
-    // console.log(options)
-    // console.log(this.resolved)
-    // // console.log(this)
-    // console.log('\n')
-
-    // // // //
-    // // // //
-
-
-    // // // //
-    // // // //
-    // // // //
-    // TODO - move this into a function ( `getModulePath`, perhaps )
-    // Handle relative paths
-    if (generatorModule.startsWith('./') || generatorModule.startsWith('../')) {
-
-      // TODO - document
-      let base = ''
-
-      // TODO - abstract into helper function?
-      const stats = fsExtra.statSync(this.resolved)
-
-      // TODO - document
-      // TODO - document
-      if (stats.isDirectory()) {
-        base = this.resolved
-      } else {
-        base = path.dirname(this.resolved)
-      }
-
-      // TODO - document
-      modulePath = path.join(base, generatorModule)
-
-    // Handle absolute path
-    // } else if (generatorModule.absolute_path) {
-    } else if (generatorModule.startsWith('/')) {
-
-      modulePath = path.join(generatorModule)
-
-    // Handle module path
-    } else {
-      modulePath = path.join(this.options.meta.engine_path, 'node_modules', generatorModule)
-    }
-    // // // //
-    // // // //
-
-    try {
-      // console.log(modulePath)
-
-      // TODO - document
-      // TODO - document
-      const generatorPrototype = require(modulePath); // eslint-disable-line import/no-dynamic-require
-      generatorPrototype.resolved = require.resolve(modulePath);
-
-      // TODO - document
-      // TODO - document
-      // TODO - move into independent function, `getResolvedGeneratorPath`, perhaps
-      let resolvedGeneratorPath = generatorPrototype.resolved.split('/')
-      resolvedGeneratorPath.pop()
-      resolvedGeneratorPath = resolvedGeneratorPath.join('/')
-
-      // // // //
-      // // // //
-      // TODO - move into independent function, `resolveDestination`, perhaps
-      // TODO - document
-      // TODO - document
-      // TODO - document
-      let resolvedDestination = this.options.dest
-      if (options.scope) {
-        resolvedDestination = path.resolve(this.options.dest, options.scope)
-      }
-
-      // // // //
-      // // // //
-      // TODO - add verbose logging function call here
-
-      // console.log('\n')
-      // console.log('this.options.dest')
-      // console.log(this.options.dest)
-      // console.log('\n')
-      // console.log('RESOLVEDDESTINATION')
-      // console.log(resolvedDestination)
-
-      // // // //
-      // // // //
-
-      // Creates new instance of generatorPrototype
-      // TODO - document this a bit more
-      const generator = new CodotypeGenerator(generatorPrototype, {
-        ...this.options,
-        dest: resolvedDestination,
-        resolved: resolvedGeneratorPath
-      })
-
-      // Invokes `generator.forEachSchema` once for each in blueprint.schemas
-      await Promise.all(this.options.blueprint.schemas.map((schema) => generator.forEachSchema({ schema: schema, ...this.options })))
-
-      // Invokes `generator.write()` once
-      await generator.write(this.options)
-
-      // Invokes generator.compileTemplatesInPlace()
-      await generator.compileTemplatesInPlace()
-
-      // Logs output
-      // TODO - add --verbose flag to conditionally print this output
-      console.log(`Generated ${generatorPrototype.name}.\n`)
-
-      // Logs which generator is being run
-    } catch (err) {
-      if (err.code === 'MODULE_NOT_FOUND') {
-        console.log('MODULE NOT FOUND')
-      } else {
-        console.log('OTHER ERROR')
-        console.log(err)
-        throw err;
-      }
-    }
-
+  async composeWith (generatorModule, options) {
+    return this.runtime.composeWith(this, generatorModule, options)
   }
-
 }
--- packages/@codotype/ui/test/index.js
+++ packages/@codotype/ui/test/index.js
@@ -3,11 +3,6 @@ const chai = require("chai");
 chai.should();
 
 // Import all library tests here
-// require('../lib/buildDefault.spec');
-// require('../lib/indent.spec');
-// require('../lib/inflateMeta.spec');
-// require('../lib/sanitizeLabel.spec');
-// require('../lib/trailingComma.spec');
 
 // // // //
 
