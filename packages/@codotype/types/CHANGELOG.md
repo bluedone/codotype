92c6d38b80a1073a8f8e11bda682b937ab4035ba
Alexander Schwartzberg <aeksco@gmail.com>
2019-03-29 23:39:39 -0400

    Updated @codotype/types to 0.2.0

--- packages/@codotype/types/CHANGELOG.md
+++ packages/@codotype/types/CHANGELOG.md
@@ -1,8 +1,19 @@
+### v0.2.0
+
+#### Features
+* Improved iconography and descriptions for datatypes
+
+#### Deprecations
+* Deprecated `DATATYPE_DOUBLE` type - merged functionality with `DATATYPE_FLOAT`
+
+---
+
 ### v0.1.1
 
 #### Features
 * Added basic testing infrastructure with Mocha, Chai, & NYC
 
+---
 
 ### v0.1.0
 
--- packages/@codotype/types/lib/relation-meta.js
+++ packages/@codotype/types/lib/relation-meta.js
@@ -5,8 +5,8 @@ const {
 } = require('./relation-types')
 
 const RELATION_META = {}
-RELATION_META[RELATION_TYPE_BELONGS_TO] = { id: RELATION_TYPE_BELONGS_TO, label: 'Many To One', description: 'A two-way association to a single model' },
-RELATION_META[RELATION_TYPE_HAS_ONE] = { id: RELATION_TYPE_HAS_ONE, label: 'One To One', description: 'A one-way association to a single model' },
-RELATION_META[RELATION_TYPE_HAS_MANY] = { id: RELATION_TYPE_HAS_MANY, label: 'One To Many', description: 'A one-way association to many models' }
+RELATION_META[RELATION_TYPE_BELONGS_TO] = { id: RELATION_TYPE_BELONGS_TO, label: 'Many To One', description: 'A one-way association to a single Schema' },
+RELATION_META[RELATION_TYPE_HAS_ONE] = { id: RELATION_TYPE_HAS_ONE, label: 'One To One', description: 'A one-way association to a single Schema' },
+RELATION_META[RELATION_TYPE_HAS_MANY] = { id: RELATION_TYPE_HAS_MANY, label: 'One To Many', description: 'A one-way association to many Schemas' }
 
 module.exports = RELATION_META
