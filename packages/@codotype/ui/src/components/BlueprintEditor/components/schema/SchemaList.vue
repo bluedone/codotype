917ec829f3cbc00c350e6cd6ef4585253d06acb0
Alexander Schwartzberg <aeksco@gmail.com>
2019-05-18 23:15:34 -0400

    Sortable SchemaList

--- packages/@codotype/ui/src/components/BlueprintEditor/components/schema/SchemaList.vue
+++ packages/@codotype/ui/src/components/BlueprintEditor/components/schema/SchemaList.vue
@@ -5,27 +5,51 @@
         Schemas
       </strong>
     </div>
-    <ul class='list-group list-group-flush'>
+    <draggable
+      class='list-group list-group-flush reflow-target'
+      v-model='collection'
+      :animation="150"
+      :fallbackTolerance="100"
+    >
       <SchemaListItem
         v-for="item in collection"
         :key="item.id"
         :model="item"
       />
-    </ul>
+    </draggable>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import draggable from 'vuedraggable'
 import SchemaListItem from './SchemaListItem'
 
 export default {
   name: 'SchemaList',
   components: {
+    draggable,
     SchemaListItem
   },
-  computed: mapGetters({
-    collection: 'editor/schema/collection/items'
-  })
+  computed: {
+    collection: {
+      get () {
+        return this.$store.getters['editor/schema/collection/items']
+      },
+      set (value) {
+
+        // Ensures only truthy values
+        value = value.filter(s => !!s)
+
+        // Sets `order` attribute on each item
+        value.forEach((s, i) => { s.order = i })
+
+        // Sorts the values by `order` attribute before commiting to the Vuex store
+        // value = orderBy(value, ['order'], ['asc'])
+
+        // Commits the sorted list to the store
+        this.$store.commit('editor/schema/collection/items', value)
+      }
+    }
+  }
 }
 </script>
