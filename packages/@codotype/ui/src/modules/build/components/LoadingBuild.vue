4b18298ec76cb56a4d19d301eda90240eb9789d1
Alexander Schwartzberg <aeksco@gmail.com>
2019-05-20 23:31:44 -0400

    LoadingBuild UX improvements

--- packages/@codotype/ui/src/modules/build/components/LoadingBuild.vue
+++ packages/@codotype/ui/src/modules/build/components/LoadingBuild.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="h-100 d-flex flex-row justify-content-center align-items-center">
     <div class="col-lg-8">
-      <div class="card card-body text-center">
-        <p class="lead">Thousands of monkeys are generating your code</p>
-        <small class="text-muted">please be nice to them its their first day on the job</small>
-        <div class="row mt-4">
+      <div class="card card-body text-center shadow-sm">
+        <p class="title"><strong>Codotype</strong> is generating your codebase</p>
+        <small class="text-muted">
+          This should only take a few moments
+          <i class="far fa-laugh" />
+        </small>
+        <div class="row my-4">
           <div class="col-lg-12 text-center d-flex justify-content-center">
             <Circle8 size="80px" class='d-flex my-2'/>
           </div>
         </div>
+        <small class="text-muted">
+          I have no patience with dinosaurs.
+          <br>
+          - <strong>Adam West</strong>
+        </small>
       </div>
     </div>
   </div>
@@ -29,3 +37,9 @@ export default {
   })
 }
 </script>
+
+<style scoped>
+  p.title {
+    font-size: 1.25rem;
+  }
+</style>
