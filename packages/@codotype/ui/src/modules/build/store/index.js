b2a934adf1c1977157bbbb22dbf6b5920cbb5e22
Alexander Schwartzberg <aeksco@gmail.com>
2019-06-11 00:04:47 -0400

    Build store pulls default schemas from generatorMeta

--- packages/@codotype/ui/src/modules/build/store/index.js
+++ packages/@codotype/ui/src/modules/build/store/index.js
@@ -9,12 +9,19 @@ export default {
   namespaced: true,
   actions: {
     // NOTE - a lot of the process here may need to be replicated elsewhere - something to think about moving forward...
-    loadSteps ({ rootGetters, dispatch }, generator_id) {
+    loadSteps ({ rootGetters, dispatch, commit }, generator_id) {
       // Finds the generator by id
       const generatorMeta = rootGetters['generator/collection'].find(g => g.id === generator_id)
 
+      // Loads in any default schemas
+      const defaultSchemas = generatorMeta.defaultSchemas || []
+
+      // Updates the schema collection and selected schema
+      commit('editor/schema/collection/items', defaultSchemas, { root: true })
+      if (defaultSchemas[0]) dispatch('editor/schema/selectModel', defaultSchemas[0], { root: true })
+
       // Loads the generator into the step module
-      dispatch('editor/created', {}, { root: true })
+      dispatch('editor/created', {}, { root: true }) // Could maybe ditch editor/created
       dispatch('tour/created', {}, { root: true })
       dispatch('steps/load', generatorMeta)
       dispatch('runtime/reset')
