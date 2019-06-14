f03ff24173a91f47f7efc03ee1501ad6efc09c7d
Alexander Schwartzberg <aeksco@gmail.com>
2019-06-14 03:03:58 -0400

    Lightened AttributeDatatypeChild color

--- packages/@codotype/ui/src/components/BlueprintEditor/components/attribute/AttributeDatatypeChild.vue
+++ packages/@codotype/ui/src/components/BlueprintEditor/components/attribute/AttributeDatatypeChild.vue
@@ -1,16 +1,16 @@
 
 <template>
   <div class="col-sm-6 my-2">
-    <button class='btn btn-outline-dark btn-block text-left active' v-if="opt.value === val" @click="click(opt.value)">
+    <button class='btn btn-outline-secondary btn-block text-left active' v-if="opt.value === val" @click="click(opt.value)">
       <i :class="'fa-fw mr-2 ' + opt.icon"></i>
       <strong>{{opt.label}}</strong>
-      <small class='ml-2 text-muted'>{{opt.desc}}</small>
+      <small class='ml-2'>{{opt.desc}}</small>
     </button>
 
-    <button class='btn btn-outline-dark btn-block text-left' v-else @click="click(opt.value)">
+    <button class='btn btn-outline-secondary btn-block text-left' v-else @click="click(opt.value)">
       <i :class="'fa-fw mr-2 ' + opt.icon"></i>
       <strong>{{opt.label}}</strong>
-      <small class='ml-2 text-muted'>{{opt.desc}}</small>
+      <small class='ml-2'>{{opt.desc}}</small>
     </button>
   </div>
 </template>
