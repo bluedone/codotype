215d2f4d7f4be32b8f53fc5fefacb05211a9c639
Alexander Schwartzberg <aeksco@gmail.com>
2019-06-22 14:35:22 -0400

    Added NavbarWrapper, NavbarBrand, NavbarLinks components

---
+++ packages/@codotype/ui/src/components/Navbar/NavbarBrand.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="d-flex flew-row align-items-center">
+    <img
+      class="navbar-logo"
+      src="https://res.cloudinary.com/codotype/image/upload/v1560045005/tech-logos/codotype.png"
+      alt="Codotype Logo"
+    />
+    <b-navbar-brand :to="url">
+      <h3 class="gradient-mask mb-0">codotype</h3>
+    </b-navbar-brand>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavbarBrand',
+  props: {
+    url: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/style.scss';
+.gradient-mask {
+  // -webkit-text-fill-color: transparent;
+  // -webkit-background-clip: text;
+  // background-clip: text;
+  // background-image: -webkit-gradient(linear, left top, right top, from($brand-primary), to($brand-secondary));
+  color: #555555;
+}
+
+.navbar-logo {
+  margin-right: .5rem;
+  margin-top: 0.25rem;
+  height: 1.5rem;
+  width: 1.5rem;
+  float: left;
+}
+</style>
---
+++ packages/@codotype/ui/src/components/Navbar/NavbarWrapper.vue
@@ -0,0 +1,31 @@
+<template>
+  <b-navbar toggleable="md" type="light" variant="light" class="bg-white">
+    <div class="container">
+      <slot name="brand"></slot>
+      <b-navbar-toggle target="nav_collapse" />
+      <b-collapse id="nav_collapse" is-nav>
+        <slot name="links"></slot>
+      </b-collapse>
+    </div>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/style.scss';
+.navbar {
+  font-family: $heading-font;
+  transition: all 0.3s ease;
+  padding: 0.4rem;
+  background: #ffffff !important;
+  box-shadow: 0px 9px 68px 0px rgba(62, 57, 107, 0.1);
+  // border-bottom: 1px solid rgba( $white, .2);
+  // border-bottom: 1px solid #d3d3d3;
+  // padding: 1rem !important;
+}
+</style>
