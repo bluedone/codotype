7c91d3a48b2d91866efcd6468ec114194f1b667b
Alexander Schwartzberg <aeksco@gmail.com>
2019-11-17 22:13:12 -0500

    Updated page layouts in generator-editor Next.js app

--- packages/@codotype/generator-editor/pages/_app.tsx
+++ packages/@codotype/generator-editor/pages/_app.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import App, { Container } from "next/app";
-// import { Container as SemanticUIContainer } from "semantic-ui-react";
-// import Navbar from "../components/Navbar";
-// import Footer from "../components/Footer";
 
 export default class extends App {
   render() {
