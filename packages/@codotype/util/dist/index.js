0239b1c768580207b6ffcc3d167b93bb7a2ba5a6
Alexander Schwartzberg <aeksco@gmail.com>
2019-11-22 23:04:53 -0500

    Added tsconfig.build.json, tsconfig.json, correct package.json exports, src/index.ts to @codotype/util - includes basic jest test

---
+++ packages/@codotype/util/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function goFish() {
+    return false;
+}
+exports.goFish = goFish;
+//# sourceMappingURL=index.js.map
---
+++ packages/@codotype/util/src/__tests__/test_util.ts
@@ -0,0 +1,5 @@
+import meaningOfLife from "@codotype/types";
+
+test("testing @codotype/util", () => {
+  expect(meaningOfLife).toBe(42);
+})
