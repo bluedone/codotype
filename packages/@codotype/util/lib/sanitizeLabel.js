43ef45b1d46504661fa636157716bda2169744b4
Alexander Schwartzberg <aeksco@gmail.com>
2019-11-24 15:30:58 -0500

    Deleted old JS source for @codotype/util

--- packages/@codotype/util/lib/sanitizeLabel.js
+++
@@ -1,9 +0,0 @@
-const titleize = require('underscore.string/titleize')
-
-// // // //
-
-module.exports = function (label) {
-  // Replaces all non letter & whitespace characters
-  // Replaces all chains of whitespace to a single space each
-  return titleize(label.replace(/[^a-zA-Z\s]/gi, '').replace(/\s\s+/g, ' '));
-}
