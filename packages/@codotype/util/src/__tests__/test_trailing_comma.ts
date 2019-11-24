67ee7406e8e588f79c8fb9129f68d9a2c366a7aa
Alexander Schwartzberg <aeksco@gmail.com>
2019-11-24 14:49:26 -0500

    Migrated trailingComma test to TS

---
+++ packages/@codotype/util/src/__tests__/test_trailing_comma.ts
@@ -0,0 +1,29 @@
+import trailingComma from "../trailingComma";
+
+// // // //
+
+describe('/lib/trailingComma.js', () => {
+
+  describe('empty array', () => {
+    it('should return empty string at 0th index', () => {
+      expect(trailingComma([], 0)).toBe('')
+    });
+  });
+
+  describe('array with length 1', () => {
+    it('should return empty string at 0th index', () => {
+      expect(trailingComma([1], 0)).toBe('')
+    });
+  });
+
+  describe('array with length 2', () => {
+    it('should return comma at 0th index', () => {
+      expect(trailingComma([1, 2], 0)).toBe(',')
+    });
+
+    it('should return empty string at 1st index', () => {
+      expect(trailingComma([1, 2], 1)).toBe('')
+    });
+  });
+
+});
