3d2367b703269e2b7d5f92f2c2fa9047cee47424
Alexander Schwartzberg <aeksco@gmail.com>
2019-11-24 14:53:25 -0500

    Migrated util/validateGenerator to TS

---
+++ packages/@codotype/util/src/validateGenerator.ts
@@ -0,0 +1,36 @@
+// All required generator fields
+const requiredFields: string[] = [
+  "id",
+  "label",
+  "icon",
+  "description",
+  "tech_tags",
+  "type_tags",
+  "self_configuring",
+  "project_path",
+  "github_url",
+  "version",
+  "official",
+  "experience",
+  "defaultSchemas",
+  "defaultConfiguration",
+  "supportedDatatypes",
+  "supportedRelations",
+  "configuration_groups",
+]
+
+// validateGenerator
+// Returns an array of { valid: boolean, property: string } objects
+// representing if each required property is present in the generator's definition
+export default function validateGenerator({ generator }): { property: string, valid: boolean }[] {
+  const validations = requiredFields.map((property) => {
+    if (generator[property] !== undefined) {
+      return { property, valid: true }
+    }
+    return { property, valid: false }
+  })
+
+
+  // Returns validations
+  return validations;
+}
