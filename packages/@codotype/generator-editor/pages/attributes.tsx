eabc1020b4395743a763e8bccdd12be5c60c3389
Alexander Schwartzberg <aeksco@gmail.com>
2019-11-24 20:01:13 -0500

    Added Jest to generator-editor package

--- packages/@codotype/generator-editor/pages/attributes.tsx
+++ packages/@codotype/generator-editor/pages/attributes.tsx
@@ -1,5 +1,7 @@
 import { AttributeEditor } from "../components/attribute_editor";
+import { SchemaForm } from "../components/schema_form";
 import { Datatype } from "@codotype/types";
+import { DEFAULT_SCHEMA } from "@codotype/types";
 
 // // // //
 
@@ -7,6 +9,9 @@ export default () => {
   return (
     <div className="row mt-4">
       <div className="col-lg-12">
+        <SchemaForm
+          schema={{ ...DEFAULT_SCHEMA }}
+        />
         <AttributeEditor
           attributes={[]}
           supportedDatatypes={[Datatype.STRING, Datatype.NUMERIC]}
