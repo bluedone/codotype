9de03235acad4a73ee49e6602d85ea972d3d1f4f
Alexander Schwartzberg <aeksco@gmail.com>
2020-03-15 13:06:09 -0400

    Tightened up spacing in SchemaDetailHeader react component

--- packages/@codotype/react-components/src/components/schema_editor/SchemaDetailHeader.tsx
+++ packages/@codotype/react-components/src/components/schema_editor/SchemaDetailHeader.tsx
@@ -13,39 +13,44 @@ export function SchemaDetailHeader(props: {
 }) {
     const [showDeleteModal, showModal] = React.useState(false);
     return (
-        <div className="row mb-2">
-            <div className="col-lg-10">
-                <h4 className="mb-0">
-                    {props.schema.tokens.label} Schema
-                    <SchemaEditButton
+        <React.Fragment>
+            <div className="row d-flex align-items-center">
+                <div className="col-lg-10">
+                    <h5 className="mb-0">
+                        {props.schema.tokens.label} Schema
+                        <SchemaEditButton
+                            schema={props.schema}
+                            onClick={props.onClickEdit}
+                        />
+                    </h5>
+                </div>
+                <div className="col-lg-2 d-flex justify-content-end">
+                    <SchemaDeleteButton
                         schema={props.schema}
-                        onClick={props.onClickEdit}
+                        onClick={() => showModal(true)}
                     />
-                </h4>
-            </div>
-            <div className="col-lg-2 d-flex justify-content-end">
-                <SchemaDeleteButton
-                    schema={props.schema}
-                    onClick={() => showModal(true)}
-                />
 
-                <SchemaDeleteModal
-                    show={showDeleteModal}
-                    onClose={() => showModal(false)}
-                    onConfirmDelete={() => {
-                        showModal(false);
-                        props.onConfirmDelete();
-                    }}
-                />
+                    <SchemaDeleteModal
+                        show={showDeleteModal}
+                        onClose={() => showModal(false)}
+                        onConfirmDelete={() => {
+                            showModal(false);
+                            props.onConfirmDelete();
+                        }}
+                    />
+                </div>
             </div>
-            <div className="col-lg-12">
-                <small className="text-muted">
-                    Describe the{" "}
-                    <strong>{props.schema.tokens.label} Schema</strong> with{" "}
-                    <strong>Attributes</strong> and <strong>Relations</strong>
-                </small>
-                {/* <hr /> */}
+            <div className="row mb-2">
+                <div className="col-lg-12">
+                    <small className="text-muted">
+                        Describe the{" "}
+                        <strong>{props.schema.tokens.label} Schema</strong> with{" "}
+                        <strong>Attributes</strong> and{" "}
+                        <strong>Relations</strong>
+                    </small>
+                    {/* <hr /> */}
+                </div>
             </div>
-        </div>
+        </React.Fragment>
     );
 }
