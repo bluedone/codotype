9286cdb253eded16f826b4cf6b6fac57bb070b9a
Alexander Schwartzberg <aeksco@gmail.com>
2020-03-15 16:22:07 -0400

    Added BuildFinished react component

---
+++ packages/@codotype/react-components/src/components/build_finished/__tests__/build_finished.stories.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+import { BuildFinished } from "../component";
+import { Story } from "@src/components/dev";
+
+// // // //
+
+storiesOf("BuildFinished", module).add("S3 Download", () => {
+    return (
+        <Story>
+            <BuildFinished
+                downloadUrl={"https://google.com"}
+                responseType={"S3_DOWNLOAD"}
+            />
+        </Story>
+    );
+});
+
+storiesOf("BuildFinished", module).add("Local Path", () => {
+    return (
+        <Story>
+            <BuildFinished
+                filepath={"/foo/bar/codotype-build/project"}
+                responseType={"LOCAL_PATH"}
+            />
+        </Story>
+    );
+});
