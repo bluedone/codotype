7394d5011401a9c87421ef2c2f2001fa64d3aa6a
Alexander Schwartzberg <aeksco@gmail.com>
2020-03-17 20:58:28 -0400

    Added additional story for ChevronAnimation react components

--- packages/@codotype/react-components/src/components/chevron_animation/__tests__/chevron_animation.stories.tsx
+++ packages/@codotype/react-components/src/components/chevron_animation/__tests__/chevron_animation.stories.tsx
@@ -5,10 +5,18 @@ import { Story } from "@src/components/dev";
 
 // // // //
 
-storiesOf("ChevronAnimation", module).add("renders", () => {
+storiesOf("ChevronAnimation", module).add("active = true", () => {
     return (
         <Story>
             <ChevronAnimation active />
+        </Story>
+    );
+});
+
+storiesOf("ChevronAnimation", module).add("active = false", () => {
+    return (
+        <Story>
+            <ChevronAnimation active={false} />
         </Story>
     );
 });
