62d20c0c67f28a99d578fbd84860275078dca47a
Frank Matranga <thefrankmatranga@gmail.com>
2020-07-21 19:07:20 -0400

    Created RelationBuilder class

---
+++ packages/@codotype/types/src/factories/RelationBuilder.ts
@@ -0,0 +1,35 @@
+import { v4 as uuidv4 } from "uuid";
+import { RelationType, Relation } from "../relation";
+import { SchemaSource } from "../schema";
+
+interface RelationBuilderParams {
+  id?: string;
+  type: RelationType;
+  destinationSchemaId: string;
+  required?: boolean;
+  source?: SchemaSource;
+  sourceSchemeAlias?: string;
+  destinationSchemeAlias?: string;
+}
+
+export class RelationBuilder implements Relation {
+  id: string = uuidv4();
+  type: RelationType;
+  destinationSchemaId: string;
+  required: boolean = false;
+  source: SchemaSource = SchemaSource.USER;
+  sourceSchemaAlias: string = "";
+  destinationSchemaAlias: string = "";
+
+  constructor(params: RelationBuilderParams) {
+    this.type = params.type;
+    this.destinationSchemaId = params.destinationSchemaId;
+
+    this.id = params.id || this.id;
+    this.required = params.required || this.required;
+    this.source = params.source || this.source;
+    this.sourceSchemaAlias = params.sourceSchemeAlias || this.sourceSchemaAlias;
+    this.destinationSchemaAlias =
+      params.destinationSchemeAlias || this.destinationSchemaAlias;
+  }
+}
