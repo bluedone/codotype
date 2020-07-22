8061cfc5708cfaae6c730856ce2558b871eaad79
Frank Matranga <thefrankmatranga@gmail.com>
2020-07-22 19:22:40 -0400

    Add config group builder class, test, and export - closes #144

--- packages/@codotype/types/src/codotype.ts
+++ packages/@codotype/types/src/codotype.ts
@@ -1,14 +1,17 @@
 import { ConfigurationGroupPropertyBuilder } from "./factories/ConfigurationGroupProperty";
 import { SchemaBuilder } from "./factories/Schema";
+import { ConfigurationGroupBuilder } from "./factories/ConfigurationGroup";
 
 // // // //
 
 export interface CodotypeFactory {
+  ConfigurationGroup: typeof ConfigurationGroupBuilder;
   ConfigurationGroupProperty: typeof ConfigurationGroupPropertyBuilder;
   Schema: typeof SchemaBuilder;
 }
 
 export const Codotype: CodotypeFactory = {
+  ConfigurationGroup: ConfigurationGroupBuilder,
   ConfigurationGroupProperty: ConfigurationGroupPropertyBuilder,
   Schema: SchemaBuilder,
 };
---
+++ packages/@codotype/types/src/factories/ConfigurationGroup.ts
@@ -0,0 +1,46 @@
+import { ConfigurationGroup } from "..";
+import {
+  GroupLayoutVariant,
+  ConfigurationGroupSection,
+  ConfigurationGroupProperty,
+} from "../configuration-option-types";
+
+interface ConfigurationGroupBuilderParams {
+  label: string;
+  identifier: string;
+  description: string;
+  documentation?: string;
+  enabled?: boolean;
+  allowDisable?: boolean;
+  layoutVariant?: GroupLayoutVariant;
+  sections?: ConfigurationGroupSection[];
+  properties?: ConfigurationGroupProperty[];
+}
+
+export class ConfigurationGroupBuilder implements ConfigurationGroup {
+  label: string;
+  identifier: string;
+  description: string;
+  layoutVariant: GroupLayoutVariant = GroupLayoutVariant.LIST;
+  sections: ConfigurationGroupSection[] = [];
+  properties: ConfigurationGroupProperty[] = [];
+  documentation: string = "";
+  enabled: boolean = true;
+  allowDisable: boolean = false;
+
+  constructor(params: ConfigurationGroupBuilderParams) {
+    this.label = params.label;
+    this.identifier = params.identifier;
+    this.description = params.description;
+
+    this.layoutVariant = params.layoutVariant || this.layoutVariant;
+    this.sections = params.sections || this.sections;
+    this.properties = params.properties || this.properties;
+    this.documentation = params.documentation || this.documentation;
+    this.enabled = params.enabled !== undefined ? params.enabled : this.enabled;
+    this.allowDisable =
+      params.allowDisable !== undefined
+        ? params.allowDisable
+        : this.allowDisable;
+  }
+}
