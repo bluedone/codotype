8606689461415de07f5a81a07a4896a63096ba61
Frank Matranga <thefrankmatranga@gmail.com>
2020-07-22 19:58:55 -0400

    Add cfg section builder class, test, and export - closes #145

--- packages/@codotype/types/src/codotype.ts
+++ packages/@codotype/types/src/codotype.ts
@@ -1,17 +1,20 @@
 import { ConfigurationGroupPropertyBuilder } from "./factories/ConfigurationGroupProperty";
 import { SchemaBuilder } from "./factories/Schema";
 import { ConfigurationGroupBuilder } from "./factories/ConfigurationGroup";
+import { ConfigurationGroupSectionBuilder } from "./factories/ConfigurationGroupSection";
 
 // // // //
 
 export interface CodotypeFactory {
   ConfigurationGroup: typeof ConfigurationGroupBuilder;
+  ConfigurationGroupSection: typeof ConfigurationGroupSectionBuilder;
   ConfigurationGroupProperty: typeof ConfigurationGroupPropertyBuilder;
   Schema: typeof SchemaBuilder;
 }
 
 export const Codotype: CodotypeFactory = {
   ConfigurationGroup: ConfigurationGroupBuilder,
+  ConfigurationGroupSection: ConfigurationGroupSectionBuilder,
   ConfigurationGroupProperty: ConfigurationGroupPropertyBuilder,
   Schema: SchemaBuilder,
 };
