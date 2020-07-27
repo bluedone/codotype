c69a6e50b4f87d7211eeb5866ae2623d0fd22469
Alexander Schwartzberg <aeksco@gmail.com>
2020-07-26 21:02:49 -0400

    Cleaned up annotations in ConfigurationInput.stories.tsx

--- packages/@codotype/ui/src/components/configuration_group_input/__tests__/ConfigurationInput.stories.tsx
+++ packages/@codotype/ui/src/components/configuration_group_input/__tests__/ConfigurationInput.stories.tsx
@@ -151,7 +151,7 @@ storyCollection.add("instance", () => {
 });
 
 // // // //
-
+// Build test cases programatically from allowDisableOptions + groupVariants
 const allowDisableOptions = [false, true];
 
 const groupVariants = [
@@ -165,15 +165,16 @@ const groupVariants = [
     GroupLayoutVariant.DETAIL_6x6,
 ];
 
-const layoutVariantStoriesNew: [
-    string,
-    string,
+const layoutVariantStories: [
+    string, // Story Collection name
+    string, // Story name
     GroupLayoutVariant,
     boolean,
 ][] = [];
+
 allowDisableOptions.forEach(allowDisable => {
     groupVariants.forEach(layoutVariant => {
-        layoutVariantStoriesNew.push([
+        layoutVariantStories.push([
             `ConfigurationEditor/ConfigurationInput/${layoutVariant}`,
             `allowDisable: ${allowDisable}`,
             layoutVariant,
@@ -182,18 +183,7 @@ allowDisableOptions.forEach(allowDisable => {
     });
 });
 
-// const layoutVariantStories: [string, GroupLayoutVariant, boolean][] = [
-//     ["API Examples - TABS layoutVariant", GroupLayoutVariant.TABS, false],
-//     ["API Examples - LIST layoutVariant", GroupLayoutVariant.LIST, false],
-//     ["API Examples - DOCS_3x9 layoutVariant", GroupLayoutVariant.DOCS_3x9, false],
-//     ["API Examples - DOCS_4x8 layoutVariant", GroupLayoutVariant.DOCS_4x8, false],
-//     ["API Examples - DOCS_6x6 layoutVariant", GroupLayoutVariant.DOCS_6x6, false],
-//     ["API Examples - DETAIL_3x9 layoutVariant", GroupLayoutVariant.DETAIL_3x9, false],
-//     ["API Examples - DETAIL_4x8 layoutVariant", GroupLayoutVariant.DETAIL_4x8, false],
-//     ["API Examples - DETAIL_6x6 layoutVariant", GroupLayoutVariant.DETAIL_6x6, false],
-// ];
-
-layoutVariantStoriesNew.forEach(testCase => {
+layoutVariantStories.forEach(testCase => {
     const storyCollection = storiesOf(testCase[0], module);
 
     // Defines configurationGroup from test case
