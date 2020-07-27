1951b7dac51852660ebe2d16c387e9c85f19664e
Frank Matranga <thefrankmatranga@gmail.com>
2020-07-27 19:11:22 -0400

    Create applyStringPropertyFilters function

---
+++ packages/@codotype/util/src/applyNumberPropertyFilters.ts
@@ -0,0 +1,29 @@
+import { NumberValueFilter } from "../../types/src/property-filter";
+
+/** Maps every NumberValueFilter to its function. */
+const filterFuncs = {
+  [NumberValueFilter.integerValue]: (num) => Math.floor(num),
+  [NumberValueFilter.positiveValue]: (num) => Math.abs(num),
+  [NumberValueFilter.negativeValue]: (num) => -1 * Math.abs(num),
+};
+
+/**
+ * Apply NumberValueFilter(s) to a number value in order.
+ *
+ * @param props Options object
+ * @param props.value The number value to apply the filters to
+ * @param props.filters The array of NumberValueFilters to apply **in order**
+ * @returns Filtered number value
+ */
+export function applyNumberPropertyFilters({
+  value,
+  filters,
+}: {
+  value: number;
+  filters: NumberValueFilter[];
+}): number {
+  return filters.reduce(
+    (str, filter) => filterFuncs[String(filter)](str),
+    value
+  );
+}
