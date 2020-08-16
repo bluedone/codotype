5d6211437b38a47e9c0effad26567323c482e63f
Frank Matranga <thefrankmatranga@gmail.com>
2020-08-16 00:11:02 -0400

    Create FaqItemCard component - based on Nuxt.js version - https://github.com/codotype/codotype-www/blob/master/pages/faq.vue

---
+++ packages/@codotype/ui/src/pages/faq/FaqItemCard.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export interface FaqItem {
+    question: string;
+    answer: string;
+}
+
+interface FaqItemProps {
+    faqItem: FaqItem;
+}
+
+export function FaqItemCard({ faqItem }: FaqItemProps) {
+    return (
+        <div className="card card-body border-light shadow-sm mb-3">
+            <div className="row">
+                <div className="col col-sm-12">
+                    <p className="lead mb-0 w-100">{faqItem.question}</p>
+                </div>
+                <div className="col-sm-12 mt-2">
+                    <p className="card-text mb-2" dangerouslySetInnerHTML={{ __html: faqItem.answer }} />
+                </div>
+            </div>
+        </div>
+    );
+}
