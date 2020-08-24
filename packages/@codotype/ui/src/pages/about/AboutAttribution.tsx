1b540f41e459e09daf1f8067e1ba68b151493a60
Alexander Schwartzberg <aeksco@gmail.com>
2020-08-23 20:02:55 -0400

    Added GithubStar component to GeneratorStart + AboutAttribution

--- packages/@codotype/ui/src/pages/about/AboutAttribution.tsx
+++ packages/@codotype/ui/src/pages/about/AboutAttribution.tsx
@@ -1,4 +1,8 @@
 import * as React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { GitHubStar } from "@src/components/GitHubStar";
+import { GitHubFollow } from "@src/components/GitHubStar/GitHubStar";
 
 // // // //
 
@@ -9,22 +13,13 @@ export function AboutAttribution() {
     return (
         <div className="row">
             <div className="col-lg-12 d-flex justify-content-center">
-                {/* <gh-btns-star
-                    slug="codotype/codotype"
-                    show-count
-                ></gh-btns-star> */}
-                STAR COUNT HERE
+                <GitHubStar />
             </div>
 
             <div className="col-lg-12 d-flex justify-content-center">
                 <p className="lead mb-0 mt-4">
                     <span>Built with </span>
-                    {/* <font-awesome-icon
-                        v-if="svg"
-                        icon="heart"
-                        className="text-danger"
-                    /> */}
-                    {/* <i v-else className="fa fa-heart text-danger" /> */}
+                    <FontAwesomeIcon icon={faHeart} className="text-danger" />
                     <span className="px-1">by</span>
                     <a href="https://github.com/aeksco" target="_blank">
                         @aeksco
@@ -32,13 +27,8 @@ export function AboutAttribution() {
                 </p>
             </div>
 
-            <div className="col-lg-12 d-flex justify-content-center">
-                FOLLOW BUTTON HERE
-                {/* <gh-btns-follow
-                    className="mt-3"
-                    user="aeksco"
-                    show-count
-                ></gh-btns-follow> */}
+            <div className="col-lg-12 d-flex justify-content-center pt-3">
+                <GitHubFollow />
             </div>
         </div>
     );
