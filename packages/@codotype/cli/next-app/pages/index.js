fab97c16a17099db06059d4978ce53cb7191a6c5
Alexander Schwartzberg <aeksco@gmail.com>
2020-09-07 17:52:36 -0400

    Published @codotype/cli 0.8.0-canary.16

--- packages/@codotype/cli/next-app/pages/index.js
+++ packages/@codotype/cli/next-app/pages/index.js
@@ -1,16 +1,31 @@
 import * as React from "react";
-import { AppNavbar } from "@codotype/ui";
-import { LocalRuntime } from "@codotype/ui/dist/src/pages/web_runtime/LocalRuntime";
+import { AppNavbar } from "@codotype/ui/dist/src/components/navbar";
+import dynamic from "next/dynamic";
+import Head from "next/head";
+
+const LocalRuntime = dynamic(
+  import("@codotype/ui/dist/src/pages/web_runtime/LocalRuntime").then(
+    (mod) => mod.LocalRuntime
+  ),
+  {
+    ssr: false,
+  }
+);
 
 // // // //
 
 export default () => {
   return (
-    <div>
+    <React.Fragment>
+      <Head>
+        <title>Codotype</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+      </Head>
       <AppNavbar />
-      <div className="mt-4 pt-4">
-        <LocalRuntime />
-      </div>
-    </div>
+      <div className="mt-4 pt-4">{<LocalRuntime />}</div>
+    </React.Fragment>
   );
 };
