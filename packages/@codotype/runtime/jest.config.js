c22c2c559fb2a4c06c25616daf1ac789db538ebf
Alexander Schwartzberg <aeksco@gmail.com>
2020-10-25 17:12:39 -0400

    Runtime - updated jest.config.js to ignore test_state.ts, updated .gitignore to exlcude output from NodeRuntime tests

--- packages/@codotype/runtime/jest.config.js
+++ packages/@codotype/runtime/jest.config.js
@@ -4,7 +4,11 @@ module.exports = {
         "**/__tests__/**/*.+(ts|tsx|js)",
         "**/?(*.)+(spec|test).+(ts|tsx|js)",
     ],
-    testPathIgnorePatterns: ["/node_modules/", "test_state.ts"],
+    testPathIgnorePatterns: [
+        "/node_modules/",
+        "test_state.ts",
+        "/mock_plugin/",
+    ],
     transform: {
         "^.+\\.(ts|tsx)$": "ts-jest",
     },
