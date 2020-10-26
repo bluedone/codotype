1fd0c744508bbd6fed205089afa92d2d2c97eb24
Alexander Schwartzberg <aeksco@gmail.com>
2020-10-25 22:43:57 -0400

    Removed deprecated ProjectConfiguration.ts from @codotype/core

--- packages/@codotype/core/src/ProjectConfiguration.ts
+++
@@ -1,9 +0,0 @@
-import { OptionValueInstance } from "./configuration-option-types";
-
-// // // //
-
-// TODO - this should be renamed to `PluginConfiguration`
-// TODO - investigate this accepting a generic type to ensure type-safe enumeration of specific keys
-export interface PluginConfiguration {
-    [key: string]: OptionValueInstance;
-}
