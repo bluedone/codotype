1e7fe94f73ed4e4e9e7805c06aea87663587fbf4
Alexander Schwartzberg <aeksco@gmail.com>
2020-10-31 18:46:26 -0400

    Updated PropertyValidation types + interfaces

--- packages/@codotype/core/src/property-validation.ts
+++ packages/@codotype/core/src/property-validation.ts
@@ -1,46 +1,39 @@
 /**
- * StringValueValidationType
+ * StringPropertyValidationType
  * Designates different types of string validations to run against the
  * value of a specific ConfigurationGroupProperty
  */
-enum StringValueValidationType {
-    minLength = "MIN_LENGTH",
-    maxLength = "MAX_LENGTH",
+export enum StringPropertyValidationTypes {
+    minLength = "min_length",
+    maxLength = "max_length",
 }
 
 /**
- * NumberValueValidationType
+ * NumberPropertyValidationType
  * Designates different types of number validations to run against the
  * value of a specific ConfigurationGroupProperty
  */
-enum NumberValueValidationType {
-    minValue = "MIN_VALUE",
-    maxValue = "MAX_VALUE",
+export enum NumberPropertyValidationTypes {
+    minValue = "min_value",
+    maxValue = "max_value",
 }
 
 /**
- * StringValueValidation
- * Defines an interface to describe a single validation for a StringValue
+ * PropertyValidationType
+ * Defines a type alias of acceptable values for PropertyValidation.type
+ * Includes the union of values defined in StringPropertyValidationTypes & NumberPropertyValidationTypes
  */
-export interface StringValueValidation {
-    validationType: StringValueValidationType;
-    value: number;
-}
-
-/**
- * NumberValueValidation
- * Defines an interface to describe a single validation for a NumberValue
- */
-export interface NumberValueValidation {
-    validationType: NumberValueValidationType;
-    value: number;
-}
+export type PropertyValidationType =
+    | "min_length"
+    | "max_length"
+    | "min_value"
+    | "max_value";
 
 /**
  * PropertyValidation
- * Defines type union between StringValueValidation[] and NumberValueValidation[]
- * Used as the mechanism to make the PropertyValidation API available to consumers
+ * Defines an interface to describe a single validation for a NumberProperty
  */
-export type PropertyValidations =
-    | StringValueValidation[]
-    | NumberValueValidation[];
+export interface PropertyValidation {
+    type: PropertyValidationType;
+    value: number;
+}
