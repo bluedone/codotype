2c29fe101455453a696dfd43a2a8562032fe0a2c
Alexander Schwartzberg <aeksco@gmail.com>
2021-01-30 18:42:17 -0500

    Updated ProjectExportModal to use Tailwind modal

--- packages/@codotype/ui/src/components/ProjectEditor/ProjectExportButton.tsx
+++ packages/@codotype/ui/src/components/ProjectEditor/ProjectExportButton.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Dropdown } from "react-bootstrap";
-import { Modal } from "react-bootstrap";
+import { Modal } from "../Modal";
 import { ProjectInput } from "@codotype/core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
@@ -14,8 +14,9 @@ const download = require("downloadjs");
  */
 function downloadProject(projectInput: ProjectInput) {
     // Defines filename
-    const filename = `codotype-project-${projectInput.identifiers.snake
-        }-${Date.now()}.json`;
+    const filename = `codotype-project-${
+        projectInput.identifiers.snake
+    }-${Date.now()}.json`;
 
     // Defines JSON string
     const jsonString: string = JSON.stringify(projectInput, null, 4);
@@ -46,27 +47,23 @@ export function ProjectExportButton(props: { projectInput: ProjectInput }) {
             </Dropdown.Item>
 
             <Modal show={showingModal} onHide={() => showModal(false)}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Export Project</Modal.Title>
-                </Modal.Header>
+                <h3>Export Project</h3>
 
-                <Modal.Body>
-                    <p className="form-text text-muted mb-2">
-                        Export a Codotype Project stored as a .JSON file
-                    </p>
+                <p className="form-text text-muted mb-2">
+                    Export a Codotype Project stored as a .JSON file
+                </p>
 
-                    <small className="text-muted">
-                        <FontAwesomeIcon icon={faInfoCircle} className="mr-1" />
-                        The exported JSON file can be re-imported into the
-                        Codotype web UI, or used directly with the
-                        <a
-                            href="https://github.com/codotype/codotype-cli"
-                            target="_blank"
-                        >
-                            Codotype CLI
-                        </a>
-                    </small>
-                </Modal.Body>
+                <small className="text-muted">
+                    <FontAwesomeIcon icon={faInfoCircle} className="mr-1" />
+                    The exported JSON file can be re-imported into the Codotype
+                    web UI, or used directly with the
+                    <a
+                        href="https://github.com/codotype/codotype-cli"
+                        target="_blank"
+                    >
+                        Codotype CLI
+                    </a>
+                </small>
 
                 <div className="modal-footer-tw">
                     <button
