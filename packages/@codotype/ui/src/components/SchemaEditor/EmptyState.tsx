e53d7cb5f8b9a3a1ae2202a37233de793b194cf0
Alexander Schwartzberg <aeksco@gmail.com>
2021-04-10 15:54:58 -0400

    Clean up SchemaEditor empty state

--- packages/@codotype/ui/src/components/SchemaEditor/EmptyState.tsx
+++ packages/@codotype/ui/src/components/SchemaEditor/EmptyState.tsx
@@ -15,35 +15,37 @@ export function EmptyState(props: {
     const { disabled, onNewTokenChange, onKeydownEnter, onSubmit } = props;
     return (
         <div className="flex py-4 justify-center items-center">
-            <div className="col-sm-8">
-                <div className="card card-body shadow-xl">
-                    <div className="row items-center justify-center">
-                        <div className="col-lg-12">
-                            <h4 className="mb-0 text-2xl flex">
-                                New Data Model
-                            </h4>
+            <div className="grid grid-cols-5">
+                <div className="col-start-2 col-span-3">
+                    <div className="card card-body shadow-xl">
+                        <div className="grid grid-cols-12">
+                            <div className="col-span-12">
+                                <h4 className="mb-0 text-2xl flex">
+                                    New Data Model
+                                </h4>
 
-                            <SchemaForm
-                                label={""}
-                                onChange={updatedTokens => {
-                                    onNewTokenChange(updatedTokens);
-                                }}
-                                onKeydownEnter={() => {
-                                    onKeydownEnter();
-                                }}
-                            />
+                                <SchemaForm
+                                    label={""}
+                                    onChange={updatedTokens => {
+                                        onNewTokenChange(updatedTokens);
+                                    }}
+                                    onKeydownEnter={() => {
+                                        onKeydownEnter();
+                                    }}
+                                />
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-lg-12 text-right mt-2">
-                        <button
-                            className="btn btn-lg btn-primary"
-                            disabled={disabled}
-                            onClick={() => {
-                                onSubmit();
-                            }}
-                        >
-                            Create Schema
-                        </button>
+                        <div className="col-span-12 text-right mt-2">
+                            <button
+                                className="btn btn-lg btn-primary"
+                                disabled={disabled}
+                                onClick={() => {
+                                    onSubmit();
+                                }}
+                            >
+                                Create Schema
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
