ec89b6d756fad9972c98b59836855f80ca28a436
Alexander Schwartzberg <aeksco@gmail.com>
2021-04-16 16:00:14 -0400

    Bumped @codotype/ui to 0.8.0-canary.48

--- packages/@codotype/ui/src/components/AppContainer/component.tsx
+++ packages/@codotype/ui/src/components/AppContainer/component.tsx
@@ -2,8 +2,8 @@ import * as React from "react";
 import classnames from "classnames";
 import { PluginNavbar } from "../navbar/PluginNavbar";
 import { PluginMetadata } from "@codotype/core";
-// import { Head } from "next/document";
-// import { SocialMeta } from "../Meta";
+import Head from "next/head";
+import { SocialMeta } from "../Meta";
 
 // // // //
 
@@ -14,33 +14,35 @@ export function AppContainer(props: {
     const [dark, setDark] = React.useState(false);
     const { plugin } = props;
 
-    // <Head>
-    //     <title>Codotype - {plugin.content.label}</title>
-    //     <SocialMeta />
-    // </Head>
     return (
-        <div
-            className={classnames("h-full pb-32", {
-                dark: dark,
-                "bg-gray-800": dark,
-                "bg-gray-100": !dark,
-            })}
-        >
-            <PluginNavbar
-                plugin={plugin}
-                darkModeEnabled={dark}
-                toggleDarkMode={() => {
-                    setDark(!dark);
-                }}
-            />
+        <React.Fragment>
+            <Head>
+                <title>Codotype - {plugin.content.label}</title>
+                <SocialMeta />
+            </Head>
             <div
-                className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-full dark:bg-gray-800 bg-gray-100 dark:text-gray-200"
-                // style={{ backgroundColor: "#f5f6f9" }}>
+                className={classnames("h-full pb-32", {
+                    dark: dark,
+                    "bg-gray-800": dark,
+                    "bg-gray-100": !dark,
+                })}
             >
-                <div className="grid grid-cols-1">
-                    <div className="col-span-1 mt-5">{props.children}</div>
+                <PluginNavbar
+                    plugin={plugin}
+                    darkModeEnabled={dark}
+                    toggleDarkMode={() => {
+                        setDark(!dark);
+                    }}
+                />
+                <div
+                    className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-full dark:bg-gray-800 bg-gray-100 dark:text-gray-200"
+                    // style={{ backgroundColor: "#f5f6f9" }}>
+                >
+                    <div className="grid grid-cols-1">
+                        <div className="col-span-1 mt-5">{props.children}</div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </React.Fragment>
     );
 }
