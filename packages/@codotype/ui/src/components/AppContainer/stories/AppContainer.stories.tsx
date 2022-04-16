b08bd462cc5354295f8bb9445db1c4841d91008f
Alexander Schwartzberg <aeksco@gmail.com>
2022-04-16 14:08:55 -0400

    Update AppContainer stories to CSF

---
+++ packages/@codotype/ui/src/components/AppContainer/stories/AppContainer.stories.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { AppContainer } from "../component";
+import { NextJsWebsiteStarterPlugin } from "../../../pages/web_runtime/__tests__/test_state";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+// // // //
+
+export default {
+    title: "Components/AppContainer",
+    component: AppContainer,
+    args: {
+        plugin: NextJsWebsiteStarterPlugin,
+    },
+} as ComponentMeta<typeof AppContainer>;
+
+const Template: ComponentStory<typeof AppContainer> = args => (
+    <AppContainer {...args}>
+        <p>Plugin Component Goes Here</p>
+    </AppContainer>
+);
+
+// // // //
+
+export const Render = Template.bind({});
