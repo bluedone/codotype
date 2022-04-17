360dd0b49a3f36b8dda5831eac6c06b43cbdd630
Alexander Schwartzberg <aeksco@gmail.com>
2022-04-16 21:52:11 -0400

    Update CopyToClipboard to CSF

--- packages/@codotype/ui/src/components/CopyToClipboard/component.tsx
+++ packages/@codotype/ui/src/components/CopyToClipboard/component.tsx
@@ -1,26 +1,32 @@
 import * as React from "react";
-const copy = require("copy-to-clipboard");
 
 // // // //
 
-export function CopyToClipboard(props: {
-    text: string;
-    children: (childProps: { copyToClipboard: () => void }) => React.ReactNode;
-    onCopy: (onCopyProps: { text: string; copied: boolean }) => void;
-    options?: {
-        debug?: boolean;
-        message?: string;
-        format?: string;
-    };
-}) {
+/**
+ * Exposes navigator.writeText via React hook
+ */
+export function useCopyToClipboard(
+    callback?: () => void,
+): [(textToCopy: string) => void] {
     // Defines copyToClipboard function passed to props.children
-    function copyToClipboard() {
-        // Copy props.text to clipboard
-        const result: boolean = copy(props.text, props.options);
-
-        // Invokes props.onCopy callback
-        props.onCopy({ text: props.text, copied: result });
+    function copyToClipboard(textToCopy: string) {
+        navigator.clipboard.writeText(textToCopy);
+        if (callback) callback();
     }
+
+    return [copyToClipboard];
+}
+
+/**
+ * Exposes navigator.writeText via childProps with optiona onCopy callback
+ */
+export function CopyToClipboard(props: {
+    onCopy?: () => void;
+    children: (childProps: {
+        copyToClipboard: (textToCopy: string) => void;
+    }) => React.ReactNode;
+}) {
+    const [copyToClipboard] = useCopyToClipboard(props.onCopy);
 
     return (
         <React.Fragment>{props.children({ copyToClipboard })}</React.Fragment>
