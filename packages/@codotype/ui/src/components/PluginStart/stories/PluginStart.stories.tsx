4c030a2546fd8b130469187b937fc66fd82a8d43
Alexander Schwartzberg <aeksco@gmail.com>
2022-04-16 21:50:43 -0400

    Update PluginStart to CSF

---
+++ packages/@codotype/ui/src/components/PluginStart/stories/PluginStart.stories.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { PluginStart } from "../component";
+import { CodotypeStoryDecorator } from "../../Story";
+import { dummyPluginMetadata } from "../../ProjectEditor/__tests__/test_state";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+// // // //
+
+export default {
+    title: "Components/PluginStart",
+    component: PluginStart,
+    args: {
+        plugin: dummyPluginMetadata,
+    },
+    parameters: {
+        layout: "fullscreen",
+    },
+    decorators: [CodotypeStoryDecorator],
+} as ComponentMeta<typeof PluginStart>;
+
+const Template: ComponentStory<typeof PluginStart> = args => (
+    <PluginStart {...args} />
+);
+
+// // // //
+
+export const Render = Template.bind({});
+
+export const WithBuildLink = Template.bind({});
+WithBuildLink.args = {
+    buildLink: "https://google.com",
+};
+
+export const WithExternalLink = Template.bind({});
+WithExternalLink.args = {
+    externalLink: "https://google.com",
+};
